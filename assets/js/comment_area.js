b4a23ccce2bf9b751ef62f6299b1c0a5f28d4e79
MakotoKonno <stefanovska.natali@gmail.com>
2017-01-23 01:41:56 +0100

    ARTICLE SECTION

---
+++ assets/js/comment_area.js
@@ -0,0 +1,10 @@
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function postComment() {
+    var comment = document.getElementById("comment_area").value ;
+    var div = document.getElementById( 'test_div' );
+    div.innerHTML = comment;
+    insertAfter(div, div);
+}
