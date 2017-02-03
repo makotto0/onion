75e6b5b9c1861714c7c1076ddf2054884479c138
MakotoKonno <stefanovska.natali@gmail.com>
2017-02-03 01:33:37 +0100

    Form validation and modal

--- assets/js/form_validation_custom.js
+++ assets/js/form_validation_custom.js
@@ -1,5 +1,5 @@
 //jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
+var current_fs, next_fs, complete_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating = false; //flag to prevent quick multi-click glitches
 
@@ -11,64 +11,9 @@ var main_phone = '';
 var main_address = '';
 var main_email = '';
 var main_password = '';
-var main_twitter = '';
-var main_facebook = '';
-var main_gplus = '';
 
-// OVA ZA POTSEKJANJE, NE E VO IGRA
 
-// $('.next').click(function () {
-//     // ova neka bidi promenliva so ke e tocna se dur ne stani netocna
-//     var successful = true;
-//     // ce da vidime sea za imejlo so se desava
-//     // zemame vrednosta
-//     var email = $(this).val();
-//     // regex od internet najden testiran ako fajka da sme zivi i zdravi
-//     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
-//     // proverka so funkcija za regex na vrednosta od poleto email
-//     if (email != '') {
-//         if (!re.test(email)) {
-//             // ovde pokazva eror za imejlo dena ne e po formato od regexo, mora da go zadovolva
-//             $('.error').show();
-//             successful = false;
-//         }
-//     }else{
-//         // eror deka nemas nisto vneseno za imejl
-//         successful = false
-//     }
-//     // ovde za pasvordite istite sranja
-//     var re_pass = "nekoj soodveten regex"";
-//     var password = $(this).val();
-//     var password_confirm = $(this).val();
-//     if ((password != '') && (password_confirm != '')) {
-//         if (re.test(password)) {
-//             if (password == password_confirm) {
-//                 // ovde realno nisto si prodolzva ili success
-//                 $('.msg').hide();
-//                 $('.success').show();
-//             } else {
-//                 // ovde error poraki za razlicni pasvordi
-//                 $('.msg').hide();
-//                 $('.error').show();
-//             }
-//         } else {
-//             // ovde errori za regex za pasvord, demek treba da vnesi soodveten pass
-//         }
-//     } else {
-//         // ovde ako fali password, mozi nahnadno da se proveri koj fali i na nego da se zatakvoat poraki erorski
-//         $('.msg').hide();
-//         $('.error').show();
-//     }
-//
-//
-//     // na krajo
-//     if (successful) {
-//         // dom manipulacija na soodvetnite elementi
-//         // izbrisi ova forma, dojsija ebeno drugata
-//         // i kako sakaks tuka tranzici manzici
-//         var main_email = email;
-//     }
-// });
+
 
 
 $("#step_one_next").click(function(){
@@ -138,9 +83,98 @@ $("#step_one_next").click(function(){
     }
 
     if (!successful) {
-        animating = false;
+            animating = false;
+        } else {
+            // OVA CELO E SAMO ZA ANIMACIJA NEMA PROMENI
+
+            //activate next step on progressbar using the index of next_fs
+            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+            //show the next fieldset
+            next_fs.show();
+            //hide the current fieldset with style
+            current_fs.animate({opacity: 0}, {
+                step: function(now, mx) {
+                    //as the opacity of current_fs reduces to 0 - stored in "now"
+                    //1. scale current_fs down to 80%
+                    // scale = 1 - (1 - now) * 0.2;
+                    //2. bring next_fs from the right(50%)
+                    left = (now * 50)+"%";
+                    //3. increase opacity of next_fs to 1 as it moves in
+                    opacity = 1 - now;
+                    // current_fs.css({'transform': 'scale('+scale+')'});
+                    next_fs.css({'left': left, 'opacity': opacity});
+                },
+                duration: 800,
+                complete: function(){
+                    current_fs.hide();
+                    animating = false;
+                },
+                //this comes from the custom easing plugin
+                easing: 'easeInOutBack'
+            });
+    }
+
+
+
+
+
+});
+
+$("#step_two_complete").click(function(){
+    var successful = true;
+
+    if(animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+
+    var email = $('#email_field').val();
+    var re_email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (email != '') {
+        if (!email.match(re_email)){
+            $('#email_error_invalid').removeClass('hide');
+            successful = false;
+        } else {
+            main_email = email;
+            $('#email_error_invalid').addClass('hide');
+        }
+        $('#email_error_empty').addClass('hide');
     } else {
-        // OVA CELO E SAMO ZA ANIMACIJA NEMA PROMENI
+        $('#email_error_empty').removeClass('hide');
+        successful = false;
+    }
+
+    var password = $('#password_field').val();
+    var re_password = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    if (password == '') {
+        if (!password.match(re_password)) {
+            $('#password_error').removeClass('hide');
+            successful = false;
+        }
+    }
+
+    var confirm_password = $('#confirm_password_field').val();
+
+    if (confirm_password != password) {
+        $('#confirm_password_error').removeClass('hide') ;
+        successful = false;
+    } else {
+        main_password = password;
+    }
+
+    if (successful) {
+        $('.modal-body').append( "<p>"+main_first_name+"</p>" );
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal();
+        })
+    }
+
+    $('#close_button').click(function(){
+        current_fs = $("#step_two_complete").parent();
+        next_fs = $("#step_one_next").parent();
 
         //activate next step on progressbar using the index of next_fs
         $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
@@ -152,12 +186,12 @@ $("#step_one_next").click(function(){
             step: function(now, mx) {
                 //as the opacity of current_fs reduces to 0 - stored in "now"
                 //1. scale current_fs down to 80%
-                scale = 1 - (1 - now) * 0.2;
+                // scale = 1 - (1 - now) * 0.2;
                 //2. bring next_fs from the right(50%)
                 left = (now * 50)+"%";
                 //3. increase opacity of next_fs to 1 as it moves in
                 opacity = 1 - now;
-                current_fs.css({'transform': 'scale('+scale+')'});
+                // current_fs.css({'transform': 'scale('+scale+')'});
                 next_fs.css({'left': left, 'opacity': opacity});
             },
             duration: 800,
@@ -168,15 +202,12 @@ $("#step_one_next").click(function(){
             //this comes from the custom easing plugin
             easing: 'easeInOutBack'
         });
-    }
-
-
-
+        // $('#step_two_complete').hide("slow");
+        // $('#step_one_next').show("slow");
+    })
 });
 
 
-$(".complete").click(function(){
-    //skroz na krajo
-    return false;
-})
+
+
 
